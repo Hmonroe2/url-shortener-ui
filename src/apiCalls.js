b4affc5e61cff43cee6fa324c688ec4fb4a9b135
Hunter Monroe <huntis@outlook.com>
2022-11-15 10:03:34 -0700

    Created Post call

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,28 @@ export const getUrls = () => {
   return fetch('http://localhost:3001/api/v1/urls')
       .then(response => response.json())
 }
+
+
+
+export const postData = (url , title) => {
+  const requestData = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      long_url: url, 
+      title: title
+    }),
+  };
+
+  return fetch(`http://localhost:3001/api/v1/urls`, requestData)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Not a 200 status');
+      }
+      alert('Data submitted')
+      return response.json();
+    })
+    .catch((error) => {
+      alert('Oops, something went wrong. Try again later');
+    });
+}
